c7320b0c741353a9cd8c68066ce56a006af0b886
Tewos-21 <tewodrosshimels54@gmail.com>
2025-04-25 00:08:21 -0700

    update changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { UserProvider } from "./utils/auth";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <UserProvider>
+  <BrowserRouter>
       <App />
-    </UserProvider>
-  </React.StrictMode>
+  </BrowserRouter>
 );
