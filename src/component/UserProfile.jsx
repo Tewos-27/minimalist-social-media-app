48b86c74c645dc3917b5fb548b111fe888522b64
Tewos-21 <tewodrosshimels54@gmail.com>
2025-05-06 02:15:46 -0700

    update all changes

--- src/component/UserProfile.jsx
+++ src/component/UserProfile.jsx
@@ -2,6 +2,8 @@
 export default function UserProfile({ user }) {
     return (
       <div className="user-profile">
+
+        {/* about user detail */}
         <img src={user.avatar} alt={`${user.username}'s avatar`} className="avatar" />
         <h2>{user.username}</h2>
         <p>{user.bio}</p>
