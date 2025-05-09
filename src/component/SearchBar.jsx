7609951b2c0f4317fa812b82f5f73c815f66c890
Tewos-21 <tewodrosshimels54@gmail.com>
2025-05-09 00:16:09 -0700

    update all changes

--- src/component/SearchBar.jsx
+++ src/component/SearchBar.jsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 
 export default function SearchBar({ onSearch }) {
   const [query, setQuery] = useState("");
-
   const handleSearch = () => {
     onSearch(query);
   };
