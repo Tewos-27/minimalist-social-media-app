31923b841b5703e77a8674fec50ea4101fd2b636
Tewodros Shimels <tewodrosshimels54@gmail.com>
2025-06-13 19:22:15 +0100

    update all changes

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 const Messages = () => {
   return (
     <>
-    <h1>message</h1>
+      <h2>Messages</h2>
+      <p>This is the messages page where you can view and send messages to your friends.</p>
+      <div className="messages-list">
+        {/* Placeholder for messages */}
+        <p>No messages yet. Start a conversation!</p>
+      </div>
+      <input type="text" placeholder="Type your message here..." className="message-input" />
+      <button className="send-message">Send</button>
     </>
   )
 }
