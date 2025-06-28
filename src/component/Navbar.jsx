a23a8e0459706784d62e15f86783ae5cc081898a
Tewodros Shimels <tewodrosshimels54@gmail.com>
2025-06-28 07:06:26 +0100

    update/navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,6 +2,10 @@ import React,{ useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
     {/* mapinf the navbar list */}
