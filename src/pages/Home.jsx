4b67cc851968dc847fc9961e4dfd32b35f4a350c
Tewodros Shimels <tewodrosshimels54@gmail.com>
2025-06-30 14:23:39 +0100

    update/changes on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-    <h1>home</h1>
+    <h1>
+      Welcome to the Minimalist Social Media App
+    </h1>
+    <p>
+      This is a simple social media application built with React. You can navigate through different pages using the links in the navigation bar.
+    </p>
+    
     </>
   )
 }
