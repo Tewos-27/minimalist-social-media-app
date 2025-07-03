21c2490fd42a45f67a10b9fc7a5d9230fecc550f
Tewodros Shimels <tewodrosshimels54@gmail.com>
2025-07-03 08:02:06 +0100

    add/ handle search fuction

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 const Search = () => {
   const [searchTerm, setSearchTerm] = React.useState("");
-  
+  const handleSearchChanges = (event) => {
+    setSearchTerm(event.target.value);
+    // You can add logic here to filter or search through friends based on the searchTerm
+    console.log("Searching for:", event.target.value);
+  }
   return (
     <>
       <h2>search bar</h2>
