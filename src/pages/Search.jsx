24eb79a3b3f75a7580d5f06165b125b44fc44716
Tewodros Shimels <tewodrosshimels54@gmail.com>
2025-07-03 08:03:30 +0100

    make advance the search input

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -6,10 +6,18 @@ const Search = () => {
     // You can add logic here to filter or search through friends based on the searchTerm
     console.log("Searching for:", event.target.value);
   }
+
   return (
     <>
       <h2>search bar</h2>
-      <input type="search" name="search" id="search" placeholder="search your friend" />
+      <input
+        type="text"
+        placeholder="Search friends..."
+        value={searchTerm}
+        onChange={handleSearchChanges}
+        className="search-input"
+      />
+      
     </>
   )
 }
